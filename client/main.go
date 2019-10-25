ada3159f222875ca29a35167e437a1d0660b45c5
kekemuyu <kekemuyu@126.com>
2019-10-25 23:30:09 +0800

    add client

--- client/main.go
+++ client/main.go
@@ -1,26 +1,23 @@
+//go:generate go run -tags generate gen.go
 package main
 
-import (
-	"gofile/msg"
-	"net"
-
-	"github.com/donnie4w/go-logger/logger"
-)
+// "github.com/donnie4w/go-logger/logger"
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:9000")
-	if err != nil {
-		panic(conn)
-	}
-	logger.Debug(conn, err)
-	var msg = msg.Msg{
-		Id: 0x00000001,
-	}
-	msg.Data = []byte("test.txt")
-	msg.Datalen = uint32(len(msg.Data))
-	logger.Debug(msg)
-	b, err := msg.Pack(msg)
-	logger.Debug(b)
-	conn.Write(b)
-	select {}
+	// conn, err := net.Dial("tcp", "127.0.0.1:9000")
+	// if err != nil {
+	// 	panic(conn)
+	// }
+	// logger.Debug(conn, err)
+	// var msg = msg.Msg{
+	// 	Id: 0x00000001,
+	// }
+	// msg.Data = []byte("test.txt")
+	// msg.Datalen = uint32(len(msg.Data))
+	// logger.Debug(msg)
+	// b, err := msg.Pack(msg)
+	// logger.Debug(b)
+	// conn.Write(b)
+	myweb := New(800, 600)
+	myweb.Run()
 }
---
+++ client/ui.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+
+	log "github.com/donnie4w/go-logger/logger"
+
+	"github.com/zserge/lorca"
+)
+
+type Myweb struct {
+	UI lorca.UI
+}
+
+func New(width, height int) Myweb {
+	var myweb Myweb
+	var err error
+	myweb.UI, err = lorca.New("", "", width, height)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return myweb
+}
+
+func (m *Myweb) Run() {
+
+	ui := m.UI
+	defer ui.Close()
+
+	// A simple way to know when UI is ready (uses body.onload even in JS)
+	ui.Bind("start", func() {
+		log.Debug("UI is ready")
+	})
+
+	//	ui.Bind("testdis", dis)
+	//	ui.Bind("openSerial", New)
+	//	ui.Bind("closeSerial", closeSerial)
+	//	ui.Bind("send", send)
+	//	ui.Bind("hexRecieveSet", hexRecieveSet)
+	// Load HTML.
+	// You may also use `data:text/html,<base64>` approach to load initial HTML,
+	// e.g: ui.Load("data:text/html," + url.PathEscape(html))
+
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	go http.Serve(ln, http.FileServer(FS))
+	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
+
+	//	go recieve(ui)
+	// You may use console.log to debug your JS code, it will be printed via
+	// log.Println(). Also exceptions are printed in a similar manner.
+	// ui.Eval(`
+	// 	console.log("Hello, world!");
+	// 	console.log('Multiple values:', [1, false, {"x":5}]);
+	// `)
+
+	// Wait until the interrupt signal arrives or browser window is closed
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, os.Interrupt)
+	select {
+	case <-sigc:
+	case <-ui.Done():
+	}
+
+	log.Debug("exiting...")
+}
